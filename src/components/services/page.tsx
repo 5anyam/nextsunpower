b209bcdd35a171bde408c9d1a5e1a27706d22cc4
Sanyam <you@example.comsanyamgulia@gmail.com>
2025-06-11 13:10:41 +0530

    Initial commit

--- src/components/services/page.tsx
+++ src/components/services/page.tsx
@@ -7,7 +7,7 @@ export function Services() {
         <div className="justify-center w-full">
             <div className="block mb-4 border-b border-slate-300 pb-2">
                 <h2 className="block w-full py-2 text-center text-3xl text-slate-700 transition-all">
-                    Pest Control <b className="text-primary">Services</b>
+                    Solar Panel <b className="text-primary">Services</b>
                 </h2>
             </div>
     <div className="flex flex-row justify-center">
